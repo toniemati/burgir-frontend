36b9a4046b42f089ef5a06dfca9e178dad076ba4
toniemati <matikas2000@gmail.com>
2022-11-13 08:56:35 +0100

    product details page

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,8 +1,33 @@
 <template>
-  <div>product details here</div>
+  {{ product?.name }}
 </template>
 
 <script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const product = ref({});
+
+const getProduct = async () => {
+  const response = await axios.get(
+    `http://192.168.0.34:8000/api/product/${route.params.id}`
+  );
+  const { data } = await response;
+  product.value = data;
+};
+
+onMounted(() => {
+  if (!route.params.id || isNaN(route.params.id)) {
+    alert(`Nie znaleziono produktu o id: ${route.params.id}`)
+    return router.push('/');
+  }
+
+  getProduct();
+});
 </script>
 
 <style>
