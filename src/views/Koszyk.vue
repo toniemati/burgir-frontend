e5a5e9c10f31f4b5f3d30b43b4f3219d620b5135
toniemati <matikas2000@gmail.com>
2022-11-19 13:21:10 +0100

    'logowanie' i rejestracja

--- src/views/Koszyk.vue
+++ src/views/Koszyk.vue
@@ -6,7 +6,7 @@
 
     <button
       v-if="koszyk.products.length >= 1"
-      @click="zamow()"
+      @click="goModalVisible"
       class="koszyk__zamowButton"
     >
       Zamów i zapłać
@@ -28,7 +28,13 @@
 
   <div v-if="showModal" class="modal">
     <div class="modal__content">
-      <div class="content__klient">
+      <div v-if="!option.length" class="modal__options">
+        <button @click="option = 'log'">Zaloguj się</button>
+
+        <button @click="option = 'reg'">Zarejestruj się</button>
+      </div>
+
+      <div v-else-if="option === 'log'" class="modal__login">
         <select @change="handleKlientChange">
           <option value="null" selected disabled>Wybierz klienta</option>
           <option
@@ -43,7 +49,33 @@
         </select>
       </div>
 
-      <div class="content__form">form</div>
+      <form
+        v-else-if="option === 'reg'"
+        class="modal__register"
+        @submit="handleFormSubmit"
+      >
+        <div class="register__group">
+          <input type="text" placeholder="Imię" />
+          <input type="text" placeholder="Nazwisko" />
+        </div>
+
+        <div class="register__group">
+          <input type="text" placeholder="Kraj" />
+          <input type="text" placeholder="Miasto" />
+        </div>
+
+        <div class="register__group">
+          <input type="text" placeholder="Ulica" />
+          <input type="text" placeholder="Nr. mieszkania/domu" />
+        </div>
+
+        <div class="register__group">
+          <input type="text" placeholder="Telefon" />
+          <input type="text" placeholder="E-mail" />
+        </div>
+
+        <button type="submit">Zarejestruj się</button>
+      </form>
     </div>
   </div>
 </template>
@@ -60,16 +92,44 @@ const koszyk = computed(() => store.getters.getKoszyk);
 const products = ref([]);
 const customers = ref([]);
 const showModal = ref(false);
+const option = ref("");
 
-const zamow = () => {
+const goModalVisible = () => {
   showModal.value = true;
-  // store.dispatch("zamow");
+};
+
+const zamow = (customerId) => {
+  store.dispatch("setCustomer", customerId);
+  store.dispatch("zamow");
+  showModal.value = false;
 };
 
 const handleKlientChange = (e) => {
-  store.dispatch("setCustomer", e.target.value);
-  store.dispatch("zamow");
-  showModal.value = false;
+  zamow(e.target.value);
+};
+
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = {
+    first_name: e.target[0].value,
+    last_name: e.target[1].value,
+    country: e.target[2].value,
+    city: e.target[3].value,
+    street: e.target[4].value,
+    house_number: e.target[5].value,
+    telephone: e.target[6].value,
+    email: e.target[7].value,
+  };
+
+  try {
+    const response = await axios.post(API_URL + "customers", data);
+    e.target.reset();
+    zamow(response.data.id);
+    option.value = "";
+  } catch (e) {
+    alert("Nie udało się zarejestrować, spróbuj ponownie poźniej");
+  }
 };
 
 const getProducts = async () => {
@@ -171,6 +231,106 @@ onMounted(() => {
 }
 
 .modal__content {
+  padding: 0 5px;
+  width: 100%;
+  height: max-content;
   margin-top: 10%;
+}
+
+@media only screen and (min-width: 768px) {
+  .modal__content {
+    max-width: 1000px;
+    padding: 0 20px;
+  }
+}
+
+.modal__options {
+  display: flex;
+  justify-content: center;
+  column-gap: 20px;
+}
+.modal__options button {
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  background: #f59705;
+  opacity: 1;
+  color: #eee;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+.modal__options button:hover {
+  background: #eee;
+  color: #f59705;
+}
+
+.modal__login {
+  display: flex;
+  justify-content: center;
+}
+
+.modal__register {
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+}
+
+.register__group {
+  display: flex;
+  row-gap: 10px;
+  flex-direction: column;
+}
+
+@media only screen and (min-width: 768px) {
+  .register__group {
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 10px;
+  }
+}
+
+.modal__register input {
+  width: 100%;
+  font-family: Lato;
+  outline: none;
+  background: black;
+  border: 1px solid #f59705;
+  border-radius: 5px;
+  padding: 10px 15px;
+  color: #eee;
+  font-size: 16px;
+}
+
+.modal__register input::placeholder {
+  font-family: Lato;
+  color: #eee;
+  opacity: 0.85;
+}
+
+.modal__register button {
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  background: #f59705;
+  opacity: 1;
+  color: #eee;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+@media only screen and (min-width: 768px) {
+  .modal__register button {
+    width: 300px;
+    margin: 0 auto;
+  }
+}
+
+.modal__register button:hover {
+  background: #eee;
+  color: #f59705;
 }
 </style>
